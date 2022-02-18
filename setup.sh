53bde199cfbc95530e7e8d56e4d659286c910e63
nanorohan <85023589+nanorohan@users.noreply.github.com>
2022-02-18 11:47:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"nanorohan@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
