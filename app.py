8b37e053891e8eb9e7650d8706a7c4de599fa237
nanorohan <85023589+nanorohan@users.noreply.github.com>
2022-02-18 16:36:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,28 @@ def welcome():
 
 #@app.route('/predict',methods=["Get"])
 
-header_pic = Image.open('triang.png')
-st.image(header_pic, use_column_width=True)
 
+import base64
+
+main_bg = "1.jpg"
+main_bg_ext = "jpg"
+
+side_bg = "2.jpg"
+side_bg_ext = "jpg"
+
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+   .sidebar .sidebar-content {{
+        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 def predict_note_authentication(variance,skewness,curtosis,entropy):
     
     """Let's Authenticate the Banks Note 
